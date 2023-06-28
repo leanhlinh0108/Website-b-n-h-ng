62794a71e06a3749ffc39d5d43013c1c1ed0a631
Linh <leanhlinh.bm@gmail.com>
2023-06-29 00:15:35 +0700

    asss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,42 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
 import { MenuItems } from "./Menuitems";
-import {link} from "react-router-dom";
+import { link } from "react-router-dom";
 
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Đông Trang</h1>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
 
-class Navbar extends Component{
-    state = {clicked : false};
-    handleClick = () =>{
-        this.setState({clicked:!this.state.clicked})
-    }
-    render(){
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Đông Trang</h1>
-                <div className="menu-icons" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
-                
-                {/* // calss ko để dấu chấm,js ko nhận */}
-                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}> 
-                    {
-                        // tạo component menuitems để dùng các button item linh động theo map
-                        MenuItems.map((item,index)=>{
-                            return(
-                                <li key={index}>
-                                    <a className={item.cName} href="/">
-                                    <i className={item.icon}></i>{item.title}
-                                    </a>
-                                </li>
-                            )
-                        })
-                    }
-                    <button>Sign Up</button>
-                </ul>
-            </nav>
-        )
-    }
+        {/* // calss ko để dấu chấm,js ko nhận */}
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {
+            // tạo component menuitems để dùng các button item linh động theo map
+            MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a className={item.cName} href="/">
+                    <i className={item.icon}></i>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })
+          }
+          <button>Sign Up</button>
+        </ul>
+      </nav>
+    );
+  }
 }
 export default Navbar;
